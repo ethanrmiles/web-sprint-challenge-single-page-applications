da1f26b42dfcc3f14ab252cc6440bcc3789fdbe1
Ethan Miles <ethanmilescs@gmail.com>
2022-02-25 13:55:03 -0600

    home page built out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+// import DeliveryOptions from './components/deliveryOptions'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+   <>
+    <header>
+     <div className="title">
+       <h2>Lambda Eats</h2>
+     </div>
+     <div className="nav">
+      <div className="nav-btn active" id='home'>Home</div>
+      <div className="nav-btn " id='help'>Help</div>
+     </div>
+   </header>
+   <section className="cta">
+     <h1>Your favorite food, delivered while coding</h1>
+     <button id='order-pizza'>Pizza?</button>
+   </section>
+   <section className="delivery-showcase">
+       {/* <DeliveryOptions /> */}
+   </section>
+
+   </>
   );
 };
 export default App;
---
+++ src/components/deliveryOptions.js
@@ -0,0 +1,42 @@
+// export default function DeliveryOptions () {
+//     const deliveryData = [
+//         {name: "McDonald's", 
+//         category: 'American-fast food',
+//         serves: 'burgers',
+//         wait_time: '20-30 mins',
+//         fee: '$5.99'
+//         },
+//         {name: "Sweetgreen", 
+//         category: 'Healthy',
+//         serves: 'Salads',
+//         wait_time: '30-45 mins',
+//         fee: '$4.99'
+//         },
+//         {name: "Starbucks", 
+//         category: 'Cafe-Coffee and Tea',
+//         serves: 'Breakfast and Brunch',
+//         wait_time: '10-20 mins',
+//         fee: '$3.99'
+//         }
+//         ]
+//     return (
+//     deliveryData.forEach(rest => {
+//         return (
+//             <div className='restaurant-card'>
+//             <div className='rest-name-wrapper'>
+//                 <h3>{rest.name}</h3>
+//             </div>
+//             <div className='rest-category-wrapper'>
+//                 <p>$</p>
+//                 <p>{rest.category}</p>
+//                     <p>{rest.serves}</p>
+//             </div>
+//             <div className='rest-card-info-wrapper'>
+//                 <div className='rest-card-info'>{rest.wait_time}</div>
+//                 <div className='rest-card-info'>{rest.fee}</div>
+//             </div>
+//         </div>
+//         )
+//     })
+// )
+// }
